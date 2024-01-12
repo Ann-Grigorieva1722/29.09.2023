d8f7ffbab565879e6b76ea052fd02a696bb8dbcd
Anna <113197670+Ann-Grigorieva1722@users.noreply.github.com>
2024-01-12 23:36:15 +0200

    Add files via upload

---
+++ 29.09/29.09/29.09/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _29._09
+{
+    public partial class MainWindow : Window
+    {
+        private string correctCode = "12345";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string value = button.Content.ToString();
+            if (value == "C")
+                txtDisplay.Text = "";
+            else if (value == "OK")
+            {
+                if (txtDisplay.Text == correctCode)
+                    MessageBox.Show("Сейф открыт!");
+                else
+                    MessageBox.Show("Неверный код!");
+            }
+            else
+                txtDisplay.Text += value;
+        }
+    }
+}
